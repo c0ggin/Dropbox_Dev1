623ff5a3c7ec2deff9e30504d33ff72f6402eb68
Andrew Coggin <ascoggin@crimson.ua.edu>
2018-02-25 20:13:04 -0600

    Foundation

--- Dropbox_Dev1/Program.cs
+++ Dropbox_Dev1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,50 @@ namespace Dropbox_Dev1
     {
         static void Main(string[] args)
         {
+            var task = Task.Run(getmyaccount);
+            task.Wait();
         }
+
+        static async Task<bool> getmyaccount()
+        {
+            //passing the target URL into a string. Target endpoint is below
+            //endpoint below sends back data equivilant to that in the PBA json schema
+            string url = "https://api.dropboxapi.com/2/team_log/get_events";
+
+            //instantiating a webrequest and passing the target endpoint url from above into it
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+
+            //setting appropriate headers for the HTTP request
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer teHGRgW-saAAAAAAAAAAW8gdZaBufKxOD0XS3fq7_HCmPtJlEd0gimDfib69FVDq");
+
+            request.Method = "POST";//Dropbox's API communicates using the POST methos, so that header is set appropriately
+
+            //Data requested is then fetched and stored
+            string postData = "null\n";
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] byte1 = encoding.GetBytes(postData);
+            request.ContentLength = byte1.Length;
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(byte1, 0, byte1.Length);
+            newStream.Close();
+
+            //response is called and the data is processed (into a raw string format) appropriately 
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    //process the response
+                    StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                    String responseString = reader.ReadToEnd();
+                    //response written to console
+                    Console.WriteLine(responseString);
+                    Console.ReadLine();
+                    return true;
+                }
+            }
+        }
+
     }
 }
